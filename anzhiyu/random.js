f25f8bd661a9cb6c1c715a20159c103ec937b693
Echo0622-ex <487988592@qq.com>
2025-08-20 13:18:51 +0800

    Site updated: 2025-08-20 13:18:31

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/08/01/MarkDown语法使用测试/","2025/08/01/MarkDown语法学习（自用）/","2025/07/31/第一篇博客/","2025/08/01/next博客美化/","2025/07/31/第二篇博客文章/","2025/08/20/hello-world/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
