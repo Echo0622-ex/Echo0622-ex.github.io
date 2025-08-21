87d6842b409bd1ed61719189b5aaee048383f051
Echo0622-ex <487988592@qq.com>
2025-08-21 20:20:09 +0800

    Site updated: 2025-08-21 20:19:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2025/08/01/MarkDown语法使用测试/","2025/08/20/hello-world/","2025/08/01/next博客美化/","2025/07/31/第一篇博客/","2025/08/20/测试2/","2025/08/21/如何使用网易爆米花加载夸克网盘影视资源/","2025/07/31/第二篇博客文章/","2025/08/01/MarkDown语法学习（自用）/"];function toRandomPost(){
+var posts=["2025/08/01/MarkDown语法使用测试/","2025/08/20/hello-world/","2025/08/01/MarkDown语法学习（自用）/","2025/08/01/next博客美化/","2025/08/21/如何使用网易爆米花加载夸克网盘影视资源/","2025/08/20/测试2/","2025/07/31/第一篇博客/","2025/07/31/第二篇博客文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
